984a4cddad7ca7f7008448574b8ba52d5c4e4d3f
cexperto <rattman.c@gmail.com>
2021-04-24 00:46:12 -0500

    Function that classifies input from users working

--- main.py
+++ main.py
@@ -4,3 +4,31 @@
 # try validate data
 # design database and connect DB
 
+user = 'hola'
+def run(user):
+    try:
+        float(user)
+        return print('is a number')
+    # if type(input)== int and type(input)== float:
+    #     return print('is a number')
+    except:
+        if type(user)== str:
+            # print('text')
+             
+            characters = list(filter(lambda c: c in '@´´-+*{}[]_.,;:/#$%&()=¿?¡!', user ))
+            if len(characters)>0:
+                # print(characters)
+                for items in characters:
+                    print(items)
+            else:
+                first = user[0]
+                print(first)                
+                long = len(user)
+                int(long)
+                last = user[long-1]
+                print(last)
+            # print('is a text')
+    
+
+if __name__ == '__main__':
+    run(user)
